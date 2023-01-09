f6b7180c9381c2abc1cbd46b49e92d1d5fd321da
Woon Seah <90259138+woonseah@users.noreply.github.com>
2023-01-10 06:35:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.webGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
