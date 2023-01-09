3ff649ff1b8cd15888f96d1e27bc32174c3f016a
Woon Seah <90259138+woonseah@users.noreply.github.com>
2023-01-10 06:28:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.webGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
