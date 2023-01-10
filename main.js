67738aa18abe3be914a4feba5ae59c25f4278ab8
Woon Seah <90259138+woonseah@users.noreply.github.com>
2023-01-10 10:23:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const scene = new THREE.Scene();
-const distance = 500;
+const distance = 1500;
 const camera = new THREE.OrthographicCamera( window.innerWidth/-2, window.innerWidth/2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000 );
 
 camera.rotation.x = 50*Math.PI/180;
