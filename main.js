2b6d0b7f05dd8055ad548269c916d2fc2b3e2775
Woon Seah <90259138+woonseah@users.noreply.github.com>
2023-01-10 11:18:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -157,11 +157,11 @@ function Car() {
   const cabin = new THREE.Mesh(
     new THREE.BoxBufferGeometry( 33*zoom, 24*zoom, 12*zoom ), 
     [
-      new THREE.MeshPhongMaterial( { color: 0xcccccc, flatShading: true, map: carBackTexture } ),
-      new THREE.MeshPhongMaterial( { color: 0x555555, flatShading: true, map: carFrontTexture } ),
-      new THREE.MeshPhongMaterial( { color: 0xff0000, flatShading: true, map: carRightSideTexture } ),
-      new THREE.MeshPhongMaterial( { color: 0x00ff00, flatShading: true, map: carLeftSideTexture } ),
-      new THREE.MeshPhongMaterial( { color: 0x0000ff, flatShading: true } ), // top
+      new THREE.MeshPhongMaterial( { color: color, flatShading: true, map: carBackTexture } ),
+      new THREE.MeshPhongMaterial( { color: color, flatShading: true, map: carFrontTexture } ),
+      new THREE.MeshPhongMaterial( { color: color, flatShading: true, map: carRightSideTexture } ),
+      new THREE.MeshPhongMaterial( { color: color, flatShading: true, map: carLeftSideTexture } ),
+      new THREE.MeshPhongMaterial( { color: color, flatShading: true } ), // top
       new THREE.MeshPhongMaterial( { color: color, flatShading: true } ) // bottom
     ]
   );
