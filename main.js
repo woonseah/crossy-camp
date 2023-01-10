13d4ffebbb34335c56386b98342f9ab312e8e5fa
Woon Seah <90259138+woonseah@users.noreply.github.com>
2023-01-10 23:12:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const scene = new THREE.Scene();
 const distance = 500;
-const camera = new THREE.OrthographicCamera( window.innerWidth/-2, window.innerWidth/2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000 );
 
+// Setup camera
+const camera = new THREE.OrthographicCamera( window.innerWidth/-2, window.innerWidth/2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000 );
 camera.rotation.x = 50*Math.PI/180;
 camera.rotation.y = 20*Math.PI/180;
 camera.rotation.z = 10*Math.PI/180;
-
 const initialCameraPositionY = -Math.tan(camera.rotation.x)*distance;
 const initialCameraPositionX = Math.tan(camera.rotation.y)*Math.sqrt(distance**2 + initialCameraPositionY**2);
 camera.position.y = initialCameraPositionY;
@@ -13,33 +13,19 @@ camera.position.x = initialCameraPositionX;
 camera.position.z = distance;
 
 const zoom = 1.5;
-
 const chickenSize = 15;
-
 const positionWidth = 42;
 const columns = 17;
 const boardWidth = positionWidth*columns;
-
 const stepTime = 200; // Miliseconds it takes for the chicken to take a step forward, backward, left or right
-
 let lanes;
 let currentLane;
 let currentColumn;
-
 let previousTimestamp;
 let startMoving;
 let moves;
 let stepStartTimestamp;
-
-const carFrontTexture = new Texture(40,80,[{x: 0, y: 10, w: 30, h: 60 }]);
-const carBackTexture = new Texture(40,80,[{x: 10, y: 10, w: 30, h: 60 }]);
-const carRightSideTexture = new Texture(110,40,[{x: 10, y: 0, w: 50, h: 30 }, {x: 70, y: 0, w: 30, h: 30 }]);
-const carLeftSideTexture = new Texture(110,40,[{x: 10, y: 10, w: 50, h: 30 }, {x: 70, y: 10, w: 30, h: 30 }]);
-
-const truckFrontTexture = new Texture(30,30,[{x: 15, y: 0, w: 10, h: 30 }]);
-const truckRightSideTexture = new Texture(25,30,[{x: 0, y: 15, w: 10, h: 10 }]);
-const truckLeftSideTexture = new Texture(25,30,[{x: 0, y: 5, w: 10, h: 10 }]);
-
+// lane generation 
 const generateLanes = () => [-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9].map((index) => {
     const lane = new Lane(index);
     lane.mesh.position.y = index*positionWidth*zoom;
@@ -118,15 +104,13 @@ function render() {
     //camera.lookAt(scene.position);
     renderer.render(scene, camera);
 }
-
+// Light setup
 hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
 scene.add(hemiLight)
-
 dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
 dirLight.position.set(-100, -100, 200);
 dirLight.castShadow = true;
 scene.add(dirLight);
-
 dirLight.shadow.mapSize.width = 2048;
 dirLight.shadow.mapSize.height = 2048;
 var d = 500;
@@ -134,11 +118,9 @@ dirLight.shadow.camera.left = - d;
 dirLight.shadow.camera.right = d;
 dirLight.shadow.camera.top = d;
 dirLight.shadow.camera.bottom = - d;
-
 // var helper = new THREE.CameraHelper( dirLight.shadow.camera );
 // var helper = new THREE.CameraHelper( camera );
 // scene.add(helper)
-
 backLight = new THREE.DirectionalLight(0x000000, .4);
 backLight.position.set(200, 200, 50);
 backLight.castShadow = true;
@@ -167,6 +149,11 @@ function Wheel() {
     return wheel;
 }
 
+// car stuff
+const carFrontTexture = new Texture(40,80,[{x: 0, y: 10, w: 30, h: 60 }]);
+const carBackTexture = new Texture(40,80,[{x: 10, y: 10, w: 30, h: 60 }]);
+const carRightSideTexture = new Texture(110,40,[{x: 10, y: 0, w: 50, h: 30 }, {x: 70, y: 0, w: 30, h: 30 }]);
+const carLeftSideTexture = new Texture(110,40,[{x: 10, y: 10, w: 50, h: 30 }, {x: 70, y: 10, w: 30, h: 30 }]);
 function Car() {
   const car = new THREE.Group();
   const color = vechicleColors[Math.floor(Math.random() * vechicleColors.length)];
@@ -220,12 +207,13 @@ function Car() {
   
   return car;
 }
-
+// truck stuff
+const truckFrontTexture = new Texture(30,30,[{x: 15, y: 0, w: 10, h: 30 }]);
+const truckRightSideTexture = new Texture(25,30,[{x: 0, y: 15, w: 10, h: 10 }]);
+const truckLeftSideTexture = new Texture(25,30,[{x: 0, y: 5, w: 10, h: 10 }]);
 function Truck() {
     const truck = new THREE.Group();
     const color = vechicleColors[Math.floor(Math.random() * vechicleColors.length)];
-    
-
     const base = new THREE.Mesh(
         new THREE.BoxGeometry( 100*zoom, 25*zoom, 5*zoom ), 
         new THREE.MeshLambertMaterial( { color: 0xb4c6fc, flatShading: true } )
@@ -274,7 +262,7 @@ function Truck() {
     
     return truck;  
   }
-
+// tree stuff
 function Three() {
     const three = new THREE.Group();
     
@@ -300,7 +288,7 @@ function Three() {
     
     return three;  
 }
-
+// player stuff
 function Chicken() {
     const chicken = new THREE.Group();
     
@@ -324,7 +312,7 @@ function Chicken() {
     
     return chicken;  
 }
-
+// plain road
 function Road() {
     const road = new THREE.Group();
 
@@ -347,7 +335,7 @@ function Road() {
     
     return road;
 }
-
+// plain grass
 function Grass() {
     const grass = new THREE.Group();
 
@@ -372,6 +360,7 @@ function Grass() {
     return grass;
 }
 
+// generates the random lane based on index given
 function Lane(index) {
     this.index = index;
     this.type = index <= 0 ? 'field' : laneTypes[Math.floor(Math.random()*laneTypes.length)];
